3887eb45afc7ea0242d62d02c3fdf25f49acfa6e
Amandeep Midha <amandeepm@gmx.com>
2016-02-03 19:32:52 +0100

    added test case readiness - skelton react component

---
+++ script/elevator.js
@@ -0,0 +1,64 @@
+'use strict';
+
+var React = require('react/addons');
+
+var ElevatorControl = React.createClass({
+
+  render : function(){
+    return(
+      <div id="elevator">
+        <button onClick ={this.props.localHandleClick}> Direction {this.props.localCounter}  </button>
+      </div>
+    );
+  }
+})
+
+var ElevatorFloorIndicator = React.createClass({
+  render : function(){
+    return <div>Floor: {this.props.localCounter}</div>
+  }
+})
+
+
+var Elevator = React.createClass({
+
+  changeDirection : function() { },
+
+
+  setFloorList : function() {   },
+
+  goFloor : function(fval) {
+    this.state.floor = fval
+  },
+
+  getFloor : function() {
+    return this.state.floor
+  },
+
+
+  getInitialState : function(){
+    return { floor: 0 };
+  },
+
+  handleUp : function(){
+    this.setState( {floor: this.state.floor+1})
+  },
+
+  handleDn : function(){
+    this.setState( {floor: this.state.floor-1})
+  },
+
+  render : function(){
+    return(
+    <div>
+      <label> Elevator </label>
+      <ElevatorControl localHandleClick={this.changeDirection} />
+      <ElevatorFloorIndicator localCounter={this.state.floor}/>
+    </div>
+    )
+  }
+})
+
+module.exports = ElevatorControl;
+module.exports = ElevatorFloorIndicator;
+module.exports = Elevator;
