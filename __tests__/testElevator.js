ded3d922158f8c38a8b9135a55fffe063fcf7c76
Amandeep Midha <amandeepm@gmx.com>
2016-02-03 20:14:20 +0100

    added test folder

---
+++ __tests__/testElevator.js
@@ -0,0 +1,11 @@
+jest.dontMock('../script/elevator.js');
+
+describe('ElevatorTest', function() {
+  it('checks initial elevator floor to be ground floor', function() {
+    	var React = require('react/addons');
+    	var TestUtils = React.addons.TestUtils; // Yet to grasp it
+    	var otis = TestUtils.renderIntoDocument(<Elevator/>);
+		var floorChk = TestUtils.findRenderedDOMComponentWithTag(otis, 'label');
+    	expect(floorChk.getDOMNode().textContent).toEqual('0');
+	});
+});
